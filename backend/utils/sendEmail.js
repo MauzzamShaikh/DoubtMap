83baf2c390b70367267f90ac62052f42fd3cf8d8
Mauzzam Shaikh <ms5880188@gmail.com>
2026-07-05 22:26:39 +0530

    switch to brevo smtp

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -3,28 +3,19 @@ dns.setDefaultResultOrder("ipv4first");
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
+  host: "smtp-relay.brevo.com",
   port: 587,
   secure: false,
-  requireTLS: true,
   auth: {
-    user: process.env.GMAIL_USER,
-    pass: process.env.GMAIL_PASS,
+    user: process.env.BREVO_USER,
+    pass: process.env.BREVO_PASS,
   },
-});
-
-transporter.verify(function (error, success) {
-    if (error) {
-        console.error("SMTP Verify Error:", error);
-    } else {
-        console.log("SMTP Ready");
-    }
 });
 
 async function sendEmail({ to, subject, html }) {
   try {
     const info = await transporter.sendMail({
-      from: `"Classroom Doubt Heatmap" <${process.env.GMAIL_USER}>`,
+      from: `"DoubtMap" <${process.env.BREVO_USER}>`,
       to,
       subject,
       html
